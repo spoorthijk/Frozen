0354f9ad352cc9c7fc8a6b6ec937b7fc48899206
Lucifer <itzmegagan2@gmail.com>
2025-04-23 00:01:49 +0530

    Added route for admin registration and made changes for admin login

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -2,7 +2,7 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-
+import Admin from "../models/Admin.js";
 // user registration
 export const register = async (req, res) => {
   try {
@@ -54,6 +54,23 @@ export const register = async (req, res) => {
 // user login
 export const login = async (req, res) => {
   try {
+    const {email} = req.body;
+    const adminUser = await Admin.findOne({email});
+    if(adminUser) {
+      const passwordCheck = await bcrypt.compare(req.body.password, adminUser.password);
+      if(!passwordCheck) {
+        return res.status(400).json({success:false, message:"Invalid password"});
+      }
+      const token = jwt.sign({id:adminUser._id,email:email},process.env.JWT_SECRET_KEY, {expiresIn:"1h"});
+      res.cookie("access_token",token,{
+        httpOnly:false,
+        secure:false,
+        sameSite:"lax",
+        path:"/"
+      });
+      return res.status(200).json({success:true, message:"Login successfull",user:{email:adminUser.email}})
+    }
+
     const user = await User.findOne({ email: req.body.email });
     if (!user) {
       return res.status(404).json({
@@ -99,3 +116,22 @@ export const login = async (req, res) => {
     });
   }
 };
+
+export const adminRegistration = async (req, res) => {
+  try {
+    const {email, password} = req.body;
+    const userExists = await Admin.findOne({email});
+    if(userExists) return res.status(400).json({success:false, message:"User already exists"});
+
+    const hashedPassword = await bcrypt.hash(password, await bcrypt.genSalt(10));
+    await Admin.create({
+      email,
+      password:hashedPassword
+    });
+    return res.status(201).json({success:true, message:"Admin created successfully", user:{email:email}});
+  }
+  catch(err) {
+    console.log(err.message);
+    return res.status(500).json({success:false, message:"Some error occured please try again later"});
+  }
+}
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { login, register } from '../controller/authController.js';
+import { login, register, adminRegistration } from '../controller/authController.js';
 
 
 const router = express.Router();
-
+router.post('/admin-signup',adminRegistration)
 router.post('/register',register)
 router.post('/login',login)
 export default router;
